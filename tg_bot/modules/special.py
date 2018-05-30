64bb70fdd43d1627c6836eef36ce94ebef4784c5
Maverick <maver_ck@outlook.com>
2018-05-31 02:41:08 +0800

    [Added] /runban inspired by /rban
    Sudo users can remotely ban/unban members from chats containing the bot
    Format for use: /rban <@username/userID> <chatID>
                              /runban <@username/userID> <chatID>
    
    Logs:
    * Ability to remotely unban a user
    * /runban handler
    * Cleaned up bans.py
    * Changes to /rbans per as ATechnoHazard
    * Removed unnecessary return ""

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -28,32 +28,6 @@ MESSAGES = (
     "Oof, you were born today ",
 )
 
-@run_async
-def quickscope(bot: Bot, update: Update, args: List[int]):
-    if args:
-        chat_id = str(args[1])
-        to_kick = str(args[0])
-    else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat/user")
-    try:
-        bot.kick_chat_member(chat_id, to_kick)
-        update.effective_message.reply_text("Attempted banning " + to_kick + " from" + chat_id)
-    except BadRequest as excp:
-        update.effective_message.reply_text(excp.message + " " + to_kick)
-
-
-@run_async
-def quickunban(bot: Bot, update: Update, args: List[int]):
-    if args:
-        chat_id = str(args[1])
-        to_kick = str(args[0])
-    else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat/user")
-    try:
-        bot.unban_chat_member(chat_id, to_kick)
-        update.effective_message.reply_text("Attempted unbanning " + to_kick + " from" + chat_id)
-    except BadRequest as excp:
-        update.effective_message.reply_text(excp.message + " " + to_kick)
 
 @run_async
 def snipe(bot: Bot, update: Update, args: List[str]):
@@ -133,15 +107,11 @@ def birthday(bot: Bot, update: Update, args: List[str]):
 __mod_name__ = "Special"
 
 SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
-QUICKSCOPE_HANDLER = CommandHandler("quickscope", quickscope, pass_args=True, filters=CustomFilters.sudo_filter)
-QUICKUNBAN_HANDLER = CommandHandler("quickunban", quickunban, pass_args=True, filters=CustomFilters.sudo_filter)
 GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
 SLIST_HANDLER = CommandHandler("slist", slist, filters=Filters.user(OWNER_ID))
 BIRTHDAY_HANDLER = CommandHandler("birthday", birthday, pass_args=True, filters=Filters.group)
 
 dispatcher.add_handler(SNIPE_HANDLER)
-dispatcher.add_handler(QUICKSCOPE_HANDLER)
-dispatcher.add_handler(QUICKUNBAN_HANDLER)
 dispatcher.add_handler(GETLINK_HANDLER)
 dispatcher.add_handler(SLIST_HANDLER)
 dispatcher.add_handler(BIRTHDAY_HANDLER)
