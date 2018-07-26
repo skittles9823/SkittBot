krypticallusion/FFbot

Introduce safe mode SQL and utility functions
Signed-off-by: ATechnoHazard <amolele@firehound.org>

FFbot/modules/misc.py
@@ -19,6 +19,7 @@ from FFbot.modules.helper_funcs.filters import CustomFilters
 from FFbot.modules.helper_funcs.chat_status import bot_admin, user_admin, can_restrict
 from FFbot.modules.translations.strings import tld
 import FFbot.modules.sql.users_sql as sql
+from FFbot.modules.sql.safemode_sql import set_safemode, is_safemoded
 
 RUN_STRINGS = (
     "Where do you think you're going?",
@@ -353,6 +354,33 @@ def clean(bot: Bot, update: Update):
             continue
     message.reply_text(replytext)
 
+@run_async
+@bot_admin
+@can_restrict
+@user_admin
+def safe_mode(bot: Bot, update: Update, args: List[str]):
+    chat = update.effective_chat
+    message = update.effective_message
+    if not args:
+        message.reply_text("This chat has its Safe Mode set to *{}*".format(is_safemoded(chat.id).safemode_status), parse_mode=ParseMode.MARKDOWN)
+        return
+
+    if str(args[0]).lower() in ["on", "yes"]:
+        set_safemode(chat.id, True)
+        message.reply_text("Safe Mode has been set to *{}*".format(is_safemoded(chat.id).safemode_status), parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif str(args[0]).lower() in ["off", "no"]:
+        set_safemode(chat.id, False)
+        message.reply_text("Safe Mode has been set to *{}*".format(is_safemoded(chat.id).safemode_status), parse_mode=ParseMode.MARKDOWN)
+        return
+    else:
+        message.reply_text("I only recognize the arguments `{}`, `{}`, `{}` or `{}`".format("Yes", "No", "On", "Off"), parse_mode=ParseMode.MARKDOWN)
+
+
+
+
+
 MARKDOWN_HELP = """
 Markdown is a very powerful formatting tool supported by telegram. {} has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
@@ -410,6 +438,7 @@ IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
 
 TIME_HANDLER = CommandHandler("time", get_time, pass_args=True)
 
+SAFEMODE_HANDLER = CommandHandler("safemode", safe_mode, pass_args=True)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs)
 SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, pass_args=True)
 INFO_HANDLER = DisableAbleCommandHandler("info", info, pass_args=True)
@@ -432,3 +461,4 @@ dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 dispatcher.add_handler(STATS_HANDLER)
 dispatcher.add_handler(CLEAN_HANDLER)
+dispatcher.add_handler(SAFEMODE_HANDLER)

FFbot/modules/sql/safemode_sql.py
@@ -1,0 +1,31 @@
+import threading
+from sqlalchemy import Column, String, Boolean
+from FFbot.modules.sql import SESSION, BASE
+
+class Safemode(BASE):
+    __tablename__="Safemode"
+    chat_id = Column(String(14), primary_key=True)
+    safemode_status = Column(Boolean)
+
+    def __init__(self, chat_id, safemode_status=False):
+        self.chat_id = str(chat_id) # ensure string
+        self.safemode_status = safemode_status
+
+Safemode.__table__.create(checkfirst=True)
+SAFEMODE_INSERTION_LOCK = threading.RLock()
+
+def set_safemode(chat_id, safemode_status=True):
+    with SAFEMODE_INSERTION_LOCK:
+        curr = SESSION.query(Safemode).get((str(chat_id)))
+        if curr:
+            SESSION.delete(curr)
+        switch_status = Safemode(str(chat_id), safemode_status)
+
+        SESSION.add(switch_status)
+        SESSION.commit()
+
+def is_safemoded(chat_id):
+    try:
+        return SESSION.query(Safemode).get((str(chat_id)))
+    finally:
+        SESSION.close()

skittles9823/SkittBot

tg_bot/modules/misc.py
@@ -11,11 +11,14 @@ from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
 
 from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
-from tg_bot.__main__ import GDPR
-from tg_bot.__main__ import STATS, USER_INFO
+from tg_bot.__main__ import GDPR, STATS, USER_INFO
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.extraction import extract_user
 from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, can_restrict
+from tg_bot.modules.translations.strings import tld
+import tg_bot.modules.sql.users_sql as sql
+from tg_bot.modules.sql.safemode_sql import set_safemode, is_safemoded
 
 RUN_STRINGS = (
     "Wew dat boi noped de fugg outta here.",
@@ -346,6 +349,33 @@ def gdpr(bot: Bot, update: Update):
                                         parse_mode=ParseMode.MARKDOWN)
 
 
+@run_async
+@bot_admin
+@can_restrict
+@user_admin
+def safe_mode(bot: Bot, update: Update, args: List[str]):
+    chat = update.effective_chat
+    message = update.effective_message
+    if not args:
+        message.reply_text("This chat has its Safe Mode set to *{}*".format(is_safemoded(chat.id).safemode_status), parse_mode=ParseMode.MARKDOWN)
+        return
+
+    if str(args[0]).lower() in ["on", "yes"]:
+        set_safemode(chat.id, True)
+        message.reply_text("Safe Mode has been set to *{}*".format(is_safemoded(chat.id).safemode_status), parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif str(args[0]).lower() in ["off", "no"]:
+        set_safemode(chat.id, False)
+        message.reply_text("Safe Mode has been set to *{}*".format(is_safemoded(chat.id).safemode_status), parse_mode=ParseMode.MARKDOWN)
+        return
+    else:
+        message.reply_text("I only recognize the arguments `{}`, `{}`, `{}` or `{}`".format("Yes", "No", "On", "Off"), parse_mode=ParseMode.MARKDOWN)
+
+
+
+
+
 MARKDOWN_HELP = """
 Markdown is a very powerful formatting tool supported by telegram. {} has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
@@ -403,6 +433,7 @@ IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
 
 TIME_HANDLER = CommandHandler("time", get_time, pass_args=True)
 
+SAFEMODE_HANDLER = CommandHandler("safemode", safe_mode, pass_args=True)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs)
 SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, pass_args=True)
 INFO_HANDLER = DisableAbleCommandHandler("info", info, pass_args=True)
@@ -424,3 +455,4 @@ dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 dispatcher.add_handler(STATS_HANDLER)
 dispatcher.add_handler(GDPR_HANDLER)
+dispatcher.add_handler(SAFEMODE_HANDLER)

tg_bot/modules/sql/safemode_sql.py
@@ -1,0 +1,31 @@
+import threading
+from sqlalchemy import Column, String, Boolean
+from tg_bot.modules.sql import SESSION, BASE
+
+class Safemode(BASE):
+    __tablename__="Safemode"
+    chat_id = Column(String(14), primary_key=True)
+    safemode_status = Column(Boolean)
+
+    def __init__(self, chat_id, safemode_status=False):
+        self.chat_id = str(chat_id) # ensure string
+        self.safemode_status = safemode_status
+
+Safemode.__table__.create(checkfirst=True)
+SAFEMODE_INSERTION_LOCK = threading.RLock()
+
+def set_safemode(chat_id, safemode_status=True):
+    with SAFEMODE_INSERTION_LOCK:
+        curr = SESSION.query(Safemode).get((str(chat_id)))
+        if curr:
+            SESSION.delete(curr)
+        switch_status = Safemode(str(chat_id), safemode_status)
+
+        SESSION.add(switch_status)
+        SESSION.commit()
+
+def is_safemoded(chat_id):
+    try:
+        return SESSION.query(Safemode).get((str(chat_id)))
+    finally:
+        SESSION.close()