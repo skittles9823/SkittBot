26854e4403dda57b86bab089f35c3927c04a4ca0
Skittles9823 <skat1@live.com.au>
2018-07-30 13:35:24 +1000

    remove admin_ok=True from sed

--- tg_bot/modules/sed.py
+++ tg_bot/modules/sed.py
@@ -117,7 +117,7 @@ eg: \\?.
 __mod_name__ = "Sed/Regex"
  
  
-SED_HANDLER = DisableAbleRegexHandler(r's([{}]).*?\1.*'.format("".join(DELIMITERS)), sed, friendly="sed", admin_ok=True)
+SED_HANDLER = DisableAbleRegexHandler(r's([{}]).*?\1.*'.format("".join(DELIMITERS)), sed, friendly="sed")
  
 dispatcher.add_handler(SED_HANDLER)
  
