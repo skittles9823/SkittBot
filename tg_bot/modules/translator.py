6309050d0e676eb17b5b4ce50f0c7747ac190ca4
SentinelP <32284419+AnggaR96s@users.noreply.github.com>
2019-01-11 02:53:46 +0700

    Improve

--- tg_bot/modules/translator.py
+++ tg_bot/modules/translator.py
@@ -1,15 +1,33 @@
+from typing import Optional, List
+
 from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
-from googletrans import Translator
+
+from tg_bot import dispatcher, LOGGER
 from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot import dispatcher
+
+from py_translator import Translator
+
+
 @run_async
-def translate(bot: Bot, update: Update):
-  message = update.effective_message
-  text = message.reply_to_message.text
-  translator=Translator()
-  reply_text=translator.translate(text, dest='en').text
-  reply_text="`Source: `\n"+text+"`Translation: `\n"+reply_text
-  message.reply_to_message.reply_text(reply_text)
-translate_handler = DisableAbleCommandHandler("translate", translate)
-dispatcher.add_handler(translate_handler)
+def do_translate(bot: Bot, update: Update, args: List[str]):
+    short_name = "Created By @MidukkiBot 😬"
+    msg = update.effective_message # type: Optional[Message]
+    lan = " ".join(args)
+    to_translate_text = msg.reply_to_message.text
+    translator = Translator()
+    try:
+        translated = translator.translate(to_translate_text, dest=lan)
+        src_lang = translated.src
+        translated_text = translated.text
+        msg.reply_text("Translated from {} to {}.\n {}".format(src_lang, lan, translated_text))
+    except exc:
+        msg.reply_text(str(exc))
+
+
+__help__ = """- /tr (language code) as reply to a long message.
+"""
+__mod_name__ = "Google Translate"
+
+dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
